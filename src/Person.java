611c8fa6be3d8a63d54b4b9280a9347895a4fc8d
ducdiem <chuducdiem1@gmail.com>
2024-06-19 16:39:15 +0900

    update Person.java

--- src/Person.java
+++ src/Person.java
@@ -2,7 +2,46 @@ public class Person
 {
     String name;
     int age;
-    String contac;
+    String contact;
 
-    
+
+
+    public Person(String name, int age, String contact) {
+        this.name = name;
+        this.age = age;
+        this.contact = contact;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contac) {
+        this.contact = contac;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", contac='" + contact + '\'' +
+                '}';
+    }
 }
