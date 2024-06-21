0e2e0be51d9f0b268da51f0e5e96344fb60e4a0d
ducdiem <chuducdiem1@gmail.com>
2024-06-21 10:21:52 +0900

    fileName changed

---
+++ src/d2/D21Rithmetics.java
@@ -0,0 +1,14 @@
+package d2;
+
+public class D21Rithmetics {
+    public static void main(String[] args) {
+        // 일반 적인 연산자 +.-.*,/와 %
+        int plus   = 10+20;
+        int minus = 20-10;
+        int multiply = 20*10;
+        int divide = 20/10;
+        // 나머지 구하기
+        int modulo = 15 %10;
+        System.out.println(modulo);
+    }
+}
