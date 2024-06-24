f80b89a5f93444a257de6b6502f83378c60d0d99
ducdiem <chuducdiem1@gmail.com>
2024-06-24 17:50:53 +0900

    edit JavaEnum.java

--- src/JavaOOP/JavaEnum.java
+++ src/JavaOOP/JavaEnum.java
@@ -8,6 +8,25 @@ enum  Level{
     MEDIUM,
     HIGH
 }
+
+// tạo một người dùng mới
+class User{
+    Level level;
+    int coin = 0;
+    private  String name;
+    private int age;
+    User(String name,int age){
+        this.name = name;
+        this.age = age;
+        // khi khởi tạo người dùng thì coin = 0  và level mặc định là Low
+        this.coin = 0;
+        level = Level.LOW;
+    }
+    public void showInfo(){
+        System.out.println(String.format("name = %s , age = %d , Level  = %s, coin = %d",name,age,level,coin));
+    }
+}
+
 public class JavaEnum {
 
     public static void main(String[] args) {
@@ -22,5 +41,14 @@ public class JavaEnum {
             case HIGH:
                 System.out.println("High Level");break;
         }
+
+        User user = new User("Diem",19);
+        user.showInfo();
+
+        // khi số coin lớn hơn 100 thì sẽ được thăng cấp
+        user.coin += 1000;
+        if(user.coin > 100) user.level = Level.MEDIUM;
+        user.showInfo();
+
     }
 }
