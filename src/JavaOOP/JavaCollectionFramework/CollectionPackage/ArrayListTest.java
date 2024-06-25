2bfe5425a1c36c1f85311f9f59a2ffcd007105d2
CHU DUC DIEM <112887374+diemchu@users.noreply.github.com>
2024-06-25 14:12:04 +0900

    Update ArrayListTest.java
    add package

--- src/JavaOOP/JavaCollectionFramework/CollectionPackage/ArrayListTest.java
+++ src/JavaOOP/JavaCollectionFramework/CollectionPackage/ArrayListTest.java
@@ -1,11 +1,11 @@
 package JavaOOP.JavaCollectionFramework.CollectionPackage;
 
 import java.util.Collection;
-
+import java.util.ArrayList;
 public class ArrayListTest {
     public static void main(String[] args) {
         // collection get khong co  method() get
-        Collection<Integer> col1 = new java.util.ArrayList<>();
+        Collection<Integer> col1 = new ArrayList<>();
         col1.add(1);
         System.out.println();
     }
